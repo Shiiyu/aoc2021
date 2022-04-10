7a2fdfc52a04143ebc69f74e97184c608fb88824
Shiiyu <faithiikittii@gmail.com>
2022-04-10 15:05:11 -0400

    feat: :sparkles: day 2 puzzle b

---
+++ day2b/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+fn main() {
+  let (mut forward, mut depth, mut aim) = (0, 0, 0);
+  let input = fs::read_to_string("./input.txt").unwrap_or_default();
+  let input_vec: Vec<(&str, &str)> = input.lines()
+    .map(|num| num.split_once(' ').unwrap_or_default())
+    .collect();
+
+  for i in 0..input_vec.len() {
+    let direction = input_vec[i].0;
+    let value = input_vec[i].1;
+
+    match (direction, value.parse::<i32>().unwrap_or_default()) {
+      ("forward", value) => {
+        forward += value;
+        depth += aim * value;
+      },
+      ("up", value) => aim -= value,
+      ("down", value) => aim += value,
+      (_, _) => ()
+    }
+  }
+
+  println!("Depth times horizontal: {}", forward * depth);
+}
