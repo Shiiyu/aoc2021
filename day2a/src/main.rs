4430e230b146bc73e8b065cb2f19f53888a314da
Shiiyu <faithiikittii@gmail.com>
2022-04-16 11:23:57 -0400

    refactor: :recycle: refactor day 2 puzzle 1

--- day2a/src/main.rs
+++ day2a/src/main.rs
@@ -1,18 +1,13 @@
 fn main() {
-  let (mut forward, mut depth) = (0, 0);
-  let input: Vec<(&str, &str)> = include_str!("../input.txt")
+  let (forward, depth) = include_str!("../input.txt")
     .lines()
-    .map(|num| num.split_once(' ').unwrap_or_default())
-    .collect();
-
-  for (direction, value) in input {
-    match (direction, value.parse::<i32>().unwrap_or_default()) {
-      ("forward", value) => forward += value,
-      ("up", value) => depth -= value,
-      ("down", value) => depth += value,
-      (_, _) => ()
-    }
-  }
+    .map(|num| num.split_once(' ').unwrap())
+    .fold((0, 0), |(f, d), (c, v)| match (c, v.parse::<i32>().unwrap()) {
+      ("forward", v) => (f + v, d),
+      ("up", v) => (f, d - v),
+      ("down", v) => (f, d + v),
+      _ => unreachable!()
+    });
 
   println!("Depth times horizontal: {}", forward * depth);
 }
