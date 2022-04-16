8d27e5d0d6e3065485f4c86adf0d36974c8398ab
Shiiyu <faithiikittii@gmail.com>
2022-04-16 11:25:22 -0400

    refactor: :recycle: change unwrap_or_default() to unwrap()

--- day1a/src/main.rs
+++ day1a/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     "Num of larger measurements: {}",
     include_str!("../input.txt")
       .lines()
-      .map(|num| num.parse().unwrap_or_default())
+      .map(|num| num.parse().unwrap())
       .collect::<Vec<u16>>()
       .windows(2)
       .filter(|d| d[0] < d[1])
--- day1b/src/main.rs
+++ day1b/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     "Num times sum measurements increased: {}",
     include_str!("../input.txt")
       .lines()
-      .map(|num| num.parse().unwrap_or_default())
+      .map(|num| num.parse().unwrap())
       .collect::<Vec<u16>>()
       .windows(4)
       .filter(|d| d[0] < d[3])
