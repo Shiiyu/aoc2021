f843a1f948d81b88f9cfe6e57b90c1b5b3cf7ef9
Shiiyu <faithiikittii@gmail.com>
2022-04-15 23:39:51 -0400

    refactor: :recycle: refactor day 1 puzzle b

--- day1b/src/main.rs
+++ day1b/src/main.rs
@@ -1,17 +1,12 @@
 fn main() {
-  let mut count = 0;
-  let nums: Vec<u16> =
-    include_str!("../input.txt").lines().map(|num| num.parse().unwrap_or_default()).collect();
-  let max = nums.len() - 2;
-
-  for i in 1..max {
-    let sum_one = nums[i - 1] + nums[i] + nums[i + 1];
-    let sum_two = nums[i] + nums[i + 1] + nums[i + 2];
-
-    if sum_two > sum_one {
-      count += 1;
-    }
-  }
-
-  println!("Num times sum measurements increased: {}", count);
+  println!(
+    "Num times sum measurements increased: {}",
+    include_str!("../input.txt")
+      .lines()
+      .map(|num| num.parse().unwrap_or_default())
+      .collect::<Vec<u16>>()
+      .windows(4)
+      .filter(|d| d[0] < d[3])
+      .count()
+  );
 }
