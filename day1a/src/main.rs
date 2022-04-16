da980692c75143e82ca659cde494be140856d587
Shiiyu <faithiikittii@gmail.com>
2022-04-15 23:24:08 -0400

    refactor: :recycle: refactor day 1 puzzle a code

--- day1a/src/main.rs
+++ day1a/src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
-  let mut count = 0;
-  let nums: Vec<u16> =
-    include_str!("../input.txt").lines().map(|num| num.parse().unwrap_or_default()).collect();
-
-  for i in 1..nums.len() {
-    if nums[i] > nums[i - 1] {
-      count += 1;
-    }
-  }
-
-  println!("Num of larger measurements: {}", count);
+  println!(
+    "Num of larger measurements: {}",
+    include_str!("../input.txt")
+      .lines()
+      .map(|num| num.parse().unwrap_or_default())
+      .collect::<Vec<u16>>()
+      .windows(2)
+      .filter(|d| d[0] < d[1])
+      .count()
+  );
 }
