8bb53cd94f6e400730358525958e736977c109c2
Shiiyu <faithiikittii@gmail.com>
2022-04-18 20:56:45 -0400

    refactor: :recycle: refactor day 3 puzzle a

--- day3a/src/main.rs
+++ day3a/src/main.rs
@@ -1,30 +1,27 @@
-use std::fs;
-
 fn main() {
-  let input = fs::read_to_string("./input.txt").unwrap_or_default();
-  let input_lines: Vec<&str> = input.lines().collect();
-  let length = input_lines[0].len();
-  let count = input_lines.len();
+  let input: Vec<&str> = include_str!("../input.txt").lines().collect();
+  let length = input[0].len();
+  let count = input.len();
   let mut string = String::new();
 
   for i in 0..length {
     let mut num = 0;
 
-    for entry in &input_lines {
+    for entry in &input {
       if &entry[i..i + 1] == "1" {
         num += 1;
       }
     }
 
-    if num % (count / 2) < num {
-      string = string.to_owned() + "1";
+    if num >= count - num {
+      string.push('1');
     } else {
-      string = string.to_owned() + "0";
+      string.push('0');
     }
   }
 
   let gamma = u32::from_str_radix(&string, 2).unwrap_or_default();
-  let epsilon = (2u32.pow(length.try_into().unwrap_or_default()) - 1) - gamma;
+  let epsilon = (1 << length) - 1 - gamma;
 
   println!("Power Usage: {}", gamma * epsilon);
 }
