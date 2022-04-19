566c9d9a2c66c335730f22d51199ecd61d27b730
Shiiyu <faithiikittii@gmail.com>
2022-04-18 21:05:51 -0400

    refactor: :recycle: refactor day 3 puzzle a

--- day3a/src/main.rs
+++ day3a/src/main.rs
@@ -1,10 +1,11 @@
+const LENGTH: usize = 12;
+const COUNT: usize = 1000;
+
 fn main() {
   let input: Vec<&str> = include_str!("../input.txt").lines().collect();
-  let length = input[0].len();
-  let count = input.len();
   let mut string = String::new();
 
-  for i in 0..length {
+  for i in 0..LENGTH {
     let mut num = 0;
 
     for entry in &input {
@@ -13,15 +14,15 @@ fn main() {
       }
     }
 
-    if num >= count - num {
+    if num >= COUNT - num {
       string.push('1');
     } else {
       string.push('0');
     }
   }
 
-  let gamma = u32::from_str_radix(&string, 2).unwrap_or_default();
-  let epsilon = (1 << length) - 1 - gamma;
+  let gamma = u32::from_str_radix(&string, 2).unwrap();
+  let epsilon = (1 << LENGTH) - 1 - gamma;
 
   println!("Power Usage: {}", gamma * epsilon);
 }
