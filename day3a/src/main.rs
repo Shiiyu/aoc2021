838ee916f90dc7cab697ba6ee5af2eaa5713f4d7
Shiiyu <faithiikittii@gmail.com>
2022-04-20 21:22:33 -0400

    refactor: :recycle: refactor day 3 puzzle a

--- day3a/src/main.rs
+++ day3a/src/main.rs
@@ -1,27 +1,19 @@
 const LENGTH: usize = 12;
-const COUNT: usize = 1000;
+const COUNT: u32 = 1000;
 
 fn main() {
-  let input: Vec<&str> = include_str!("../input.txt").lines().collect();
-  let mut string = String::new();
+  let input: Vec<u32> =
+    include_str!("../input.txt").lines().map(|l| u32::from_str_radix(l, 2).unwrap()).collect();
+  let mut num_vec = vec![0; LENGTH];
 
-  for i in 0..LENGTH {
-    let mut num = 0;
-
-    for entry in &input {
-      if &entry[i..i + 1] == "1" {
-        num += 1;
-      }
+  input.iter().for_each(|n| {
+    for i in 0..LENGTH {
+      num_vec[i] += (n & (1 << i)) >> i;
     }
+  });
 
-    if num >= COUNT - num {
-      string.push('1');
-    } else {
-      string.push('0');
-    }
-  }
-
-  let gamma = u32::from_str_radix(&string, 2).unwrap();
+  let gamma: u32 =
+    num_vec.into_iter().enumerate().map(|(i, n)| ((n >= (COUNT / 2)) as u32) << i).sum();
   let epsilon = (1 << LENGTH) - 1 - gamma;
 
   println!("Power Usage: {}", gamma * epsilon);
