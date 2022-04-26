c22d9c7c0548748772ef977b8384e79c67ed4e0d
Shiiyu <faithiikittii@gmail.com>
2022-04-25 20:26:53 -0400

    refactor: :recycle: refactor day 3 puzzle b

--- day3b/src/main.rs
+++ day3b/src/main.rs
@@ -1,36 +1,36 @@
 const LENGTH: usize = 12;
 
-fn life(mut values: Vec<&str>, greater: bool) -> u32 {
-  let mut count = values.len();
-  let mut life = String::new();
+fn count(vec: &Vec<u32>, i: usize) -> (u32, u32) {
+  let mut result: (u32, u32) = (0, 0);
 
-  for i in 0..LENGTH {
-    if count == 1 {
-      life = values[0].to_owned();
-
-      break;
-    }
-
-    let num = values.iter().filter(|n| &n[i..i + 1] == "1").count();
-
-    if (greater && num >= count - num) || (!greater && num < count - num) {
-      values.retain(|&x| x.starts_with(&(life.clone() + "1")));
-      life.push('1');
-    } else {
-      values.retain(|&x| x.starts_with(&(life.clone() + "0")));
-      life.push('0');
-    }
-
-    count = values.len();
+  for num in vec {
+    result.0 += (num & (1 << i)) >> i;
+    result.1 += 1
   }
 
-  u32::from_str_radix(&life, 2).unwrap_or_default()
+  result
+}
+
+fn solve(mut vec: Vec<u32>, greater: bool) -> u32 {
+  for i in (0..LENGTH).rev() {
+    let amount = count(&vec, i);
+
+    if amount.1 > 1 {
+      let which = ((amount.0 >= (amount.1 + 1) / 2) == greater) as u32;
+
+      vec.retain(|n| ((n & (1 << i)) >> i) == which);
+    } else {
+      break;
+    }
+  }
+
+  vec[0]
 }
 
 fn main() {
-  let input = include_str!("../input.txt");
-  let oxygen = life(input.lines().collect(), true);
-  let co2 = life(input.lines().collect(), false);
+  let oxy: Vec<u32> =
+    include_str!("../input.txt").lines().map(|l| u32::from_str_radix(l, 2).unwrap()).collect();
+  let co2 = oxy.clone();
 
-  println!("Life Support Rating: {}", oxygen * co2);
+  println!("Life Support Rating: {}", solve(oxy, true) * solve(co2, false));
 }
