57d908f98b0381056e3dca0b33d988763ed1949c
Shiiyu <faithiikittii@gmail.com>
2022-05-06 11:16:24 -0400

    refactor: :recycle: refactor day 5

--- day5a/src/main.rs
+++ day5a/src/main.rs
@@ -1,49 +1,160 @@
+use std::{
+  cmp::{max, min},
+  collections::HashSet
+};
+
+#[derive(Clone, Copy)]
+struct Line {
+  x: u16,
+  y: u16,
+  dx: u16,
+  dy: u16
+}
+
+impl Line {
+  fn new(s: &str) -> Self {
+    let (start, end) = s.split_once(" -> ").unwrap();
+
+    let (x, y) = start.split_once(',').map(|(x, y)| (x.parse().unwrap(), y.parse().unwrap())).unwrap();
+    let (dx, dy) = end.split_once(',').map(|(x, y)| (x.parse().unwrap(), y.parse().unwrap())).unwrap();
+
+    if x <= dx {
+      Self { x, y, dx, dy }
+    } else {
+      Self { x: dx, y: dy, dx: x, dy: y }
+    }
+  }
+
+  fn is_straight(&self) -> bool {
+    self.x == self.dx || self.y == self.dy
+  }
+
+  fn sorted_y(&self) -> (u16, u16) {
+    if self.y < self.dy {
+      (self.y, self.dy)
+    } else {
+      (self.dy, self.y)
+    }
+  }
+
+  // (a, b) from ax + by + c = 0
+  fn ab(&self) -> (f32, f32) {
+    if self.y == self.dy {
+      (0.0, 1.0)
+    } else if self.x == self.dx {
+      (1.0, 0.0)
+    } else if self.y < self.dy {
+      (-1.0, 1.0)
+    } else {
+      (1.0, 1.0)
+    }
+  }
+
+  // c from ax + by + c = 0
+  fn c(&self) -> f32 {
+    if self.y == self.dy {
+      -(self.y as f32)
+    } else if self.x == self.dx {
+      -(self.x as f32)
+    } else if self.y < self.dy {
+      self.x as f32 - self.y as f32
+    } else {
+      -(self.x as f32) - self.y as f32
+    }
+  }
+
+  fn can_layer(&self, other: &Self) -> bool {
+    if self.y == self.dy {
+      other.y == other.dy
+    } else if other.y == other.dy {
+      self.y == self.dy
+    } else if self.x == self.dx {
+      other.x == other.dx
+    } else if other.x == other.dx {
+      self.x == self.dx
+    } else {
+      (self.y < self.dy && other.y < other.dy) || (self.y > self.dy && other.y > other.dy)
+    }
+  }
+
+  fn contains(&self, x: u16, y: u16) -> bool {
+    let ys = self.sorted_y();
+
+    self.x <= x && self.dx >= x && ys.0 <= y && ys.1 >= y
+  }
+}
+
+fn layer(first: &Line, second: &Line) -> Vec<(u16, u16)> {
+  let mut list = Vec::new();
+
+  if first.x == first.dx {
+    let f = first.sorted_y();
+    let s = second.sorted_y();
+
+    for y in max(f.0, s.0)..=min(f.1, s.1) {
+      list.push((first.x, y));
+    }
+  } else if first.y == first.dy {
+    for x in max(first.x, second.x)..=min(first.dx, second.dx) {
+      list.push((x, first.y));
+    }
+  } else if first.dx >= second.x && second.dx >= first.x {
+    let start = if first.x < second.x { (second.x, second.y) } else { (first.x, first.y) };
+
+    if first.y < first.dy {
+      for i in 0..=(min(first.dx, second.dx) - start.0) {
+        list.push((start.0 + i, start.1 + i));
+      }
+    } else {
+      for i in 0..=(min(first.dx, second.dx) - start.0) {
+        list.push((start.0 + i, start.1 - i));
+      }
+    }
+  }
+
+  list
+}
+
+fn intersection(first: &Line, second: &Line) -> Option<(u16, u16)> {
+  let (a1, b1) = first.ab();
+  let (a2, b2) = second.ab();
+  let (c1, c2) = (first.c(), second.c());
+
+  let d = (a1 * b2) - (a2 * b1);
+  let x = ((b1 * c2) - (b2 * c1)) / d;
+  let y = ((c1 * a2) - (c2 * a1)) / d;
+
+  if x % 1.0 != 0.0 || y % 1.0 != 0.0 {
+    return None;
+  }
+
+  let (x, y) = (x as u16, y as u16);
+
+  if first.contains(x, y) && second.contains(x, y) {
+    Some((x, y))
+  } else {
+    None
+  }
+}
 fn main() {
-  let input: Vec<&str> = include_str!("../input.txt").lines().collect();
-  let mut grid = [[0u8; 1000]; 1000];
-  let mut count = 0u16;
+  let lines: Vec<Line> = include_str!("../input.txt").lines().map(|n| Line::new(n)).collect();
+  let mut overlaps = HashSet::new();
 
-  for i in input {
-    let mut split = i.split(" -> ");
-    let first = split.next().unwrap_or_default();
-    let second = split.next().unwrap_or_default();
-    let (x1_str, y1_str) = first.split_once(',').unwrap_or_default();
-    let (x2_str, y2_str) = second.split_once(',').unwrap_or_default();
-    let x1 = x1_str.parse::<usize>().unwrap_or_default();
-    let x2 = x2_str.parse::<usize>().unwrap_or_default();
-    let y1 = y1_str.parse::<usize>().unwrap_or_default();
-    let y2 = y2_str.parse::<usize>().unwrap_or_default();
-
-    if x1 == x2 {
-      if y1 < y2 {
-        for i in y1..=y2 {
-          grid[x1][i] += 1;
+  for (i, first) in lines.iter().enumerate() {
+    for second in lines.iter().skip(i + 1) {
+      if !first.is_straight() || !second.is_straight() {
+        continue;
+      } else if first.can_layer(second) {
+        if first.c() == second.c() {
+          layer(first, second).into_iter().for_each(|point| {
+            overlaps.insert(point);
+          });
         }
       } else {
-        for i in y2..=y1 {
-          grid[x1][i] += 1;
-        }
-      }
-    } else if y1 == y2 {
-      if x1 < x2 {
-        for i in x1..=x2 {
-          grid[i][y1] += 1;
-        }
-      } else {
-        for i in x2..=x1 {
-          grid[i][y1] += 1;
-        }
+        intersection(first, second).map(|point| overlaps.insert(point));
       }
     }
   }
 
-  for x in 0..1000 {
-    for y in 0..1000 {
-      if grid[x][y] >= 2 {
-        count += 1;
-      }
-    }
-  }
-
-  println!("{count}");
+  println!("Overlaps: {}", overlaps.len());
 }
