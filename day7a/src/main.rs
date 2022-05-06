eb47cfc2c973221f67213214be6ec6de98f781b9
Shiiyu <faithiikittii@gmail.com>
2022-05-06 10:37:35 -0400

    refactor: :recycle: refactor day 7 puzzle a

--- day7a/src/main.rs
+++ day7a/src/main.rs
@@ -1,20 +1,7 @@
 fn main() {
-  let crabs: Vec<usize> = include_str!("../input.txt").split(',').map(|n| n.parse().unwrap_or_default()).collect();
-  let min = crabs.iter().min().unwrap().clone();
-  let max = crabs.iter().max().unwrap().clone();
-  let mut min_fuel = usize::MAX;
+  let mut crabs: Vec<u16> = include_str!("../input.txt").split(',').map(|n| n.parse().unwrap_or_default()).collect();
+  crabs.sort_unstable();
+  let align = crabs[crabs.len() / 2];
 
-  for i in min..=max {
-    let mut current_fuel = 0;
-
-    crabs.iter().for_each(|&p| current_fuel += (i as isize - p as isize).abs());
-
-    let current = current_fuel.try_into().unwrap_or_default();
-
-    if current < min_fuel {
-      min_fuel = current;
-    }
-  }
-
-  println!("{min_fuel}");
+  println!("Fuel Usage: {}", crabs.iter().fold(0, |fuel, crab| fuel + crab.abs_diff(align) as u32));
 }
