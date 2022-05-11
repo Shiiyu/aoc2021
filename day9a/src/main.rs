5d3d4de5447281e8d15d9340abea0e03da6bb529
Shiiyu <faithiikittii@gmail.com>
2022-05-10 22:50:09 -0400

    refactor: :recycle: change to include bytes macro

--- day9a/src/main.rs
+++ day9a/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-  let heightmap: Vec<Vec<u8>> =
-    include_str!("../input.txt").lines().map(|l| l.chars().map(|n| n as u8 - '0' as u8).collect()).collect();
+  let heightmap: Vec<Vec<u8>> = include_bytes!("../input.txt").split(|&b| b == b'\n').map(|l| l.to_vec()).collect();
   let mut sum = 0;
 
   for y in 0..heightmap.len() {
@@ -10,7 +9,7 @@ fn main() {
         && (x == heightmap[y].len() - 1 || heightmap[y][x + 1] > heightmap[y][x])
         && (y == heightmap.len() - 1 || heightmap[y + 1][x] > heightmap[y][x])
       {
-        sum += (heightmap[y][x] + 1) as u16;
+        sum += ((heightmap[y][x] + 1) - b'0') as u16;
       }
     }
   }
